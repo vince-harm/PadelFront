70990d8cc286fef9156e4e4b8e785a240f7bf79a
vince.h <v.harmegnies@gmail.com>
2026-03-15 20:28:26 +0100

    add header.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {WelcomePage} from './views/welcome-page/welcome-page';
+import {Header} from './views/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomePage],
+  imports: [RouterOutlet, WelcomePage, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
