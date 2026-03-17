6a32324c53786a1e9dd05fcc34d88b26494eb5c8
vince.h <v.harmegnies@gmail.com>
2026-03-17 21:18:19 +0100

    add footer.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {WelcomePage} from './views/welcome-page/welcome-page';
 import {Header} from './views/layout/header/header';
+import {Footer} from './views/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomePage, Header],
+  imports: [RouterOutlet, WelcomePage, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
